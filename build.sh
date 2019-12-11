86403b26e970b722948add6583be59aef8704e3f
David Faria <davidfaria89@gmail.com>
2019-12-11 20:08:35 -0300

    add start or reload queue

--- build.sh
+++ build.sh
@@ -16,11 +16,22 @@ rsync -ahv "./src/$VIEWS/" "./dist/$VIEWS"
 # run migrate
 npx sequelize db:migrate
 
-# Start Or Reload Service
+# Start Or Reload Api
 if [ "$(pm2 id gympoint-api)" = "[]" ]; then
 	echo "Start - gympoint-api"
   pm2 start --name "gympoint-api" dist/server.js
 else
   echo "Reload - gympoint-api"
   pm2 reload gympoint-api
+fi
+
+
+
+# Start Or Reload Queue Jobs
+if [ "$(pm2 id gympoint-queue)" = "[]" ]; then
+	echo "Start - gympoint-queue"
+  pm2 start --name "gympoint-queue" dist/queue.js
+else
+  echo "Reload - gympoint-queue"
+  pm2 reload gympoint-queue
 fi
