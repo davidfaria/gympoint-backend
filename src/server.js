cab6d421591128673eb1c4a59546d3e0708b8789
David Faria <davidfaria89@gmail.com>
2020-02-04 14:14:30 -0300

    add build automatixado

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './app';
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server up! port: ${PORT}`);
+const APP_NAME = process.env.APP_NAME;
+app.server.listen(PORT, () => {
+  console.log(`${APP_NAME} up! port: ${PORT}`);
 });
